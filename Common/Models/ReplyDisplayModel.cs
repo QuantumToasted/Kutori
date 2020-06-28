7f2e778a596b5afd03ad14f36629211d87e29331
Daniel Jarski <danieljarski@gmail.com>
2020-06-28 17:04:16 -0500

    Separated ReplyDisplayModel.
    (from PostDisplayModel)

--- Common/Models/ReplyDisplayModel.cs
+++ Common/Models/ReplyDisplayModel.cs
@@ -6,8 +6,39 @@ namespace Kutori.Common
     /// <summary>
     /// Returned on a successful reply creation.
     /// </summary>
-    public class ReplyDisplayModel : PostDisplayModel
+    public class ReplyDisplayModel
     {
+        /// <summary>
+        /// The ID of the new post.
+        /// </summary>
+        /// <example>1</example>
+        [JsonPropertyName("id")]
+        [Required]
+        public int Id { get; set; }
+
+        /// <summary>
+        /// The text of the new post.
+        /// </summary>
+        /// <example>Kutori a BEST</example>
+        [JsonPropertyName("text")]
+        [Required]
+        public string Text { get; set; }
+
+        /// <summary>
+        /// The timestamp of the post, expressed as seconds since the Unix epoch.
+        /// </summary>
+        /// <example>1593284912</example>
+        [JsonPropertyName("timestamp")]
+        [Required]
+        public long Timestamp { get; set; }
+
+        /// <summary>
+        /// An RFC 2397 data URI representing the image attached to the post.
+        /// </summary>
+        /// <example>data:image/png;base64,RAW_BASE64_DATA_OF_KUTORI</example>
+        [JsonPropertyName("image")]
+        public string Image { get; set; }
+
         /// <summary>
         /// The ID of the Post this reply is for.
         /// </summary>
